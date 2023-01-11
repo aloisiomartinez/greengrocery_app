16b4a949d44f20608690243b8abb645a98a91b94
aloisiomartinez <aloisiojacobs36@gmail.com>
2023-01-11 15:07:11 -0300

    loading with shimmer

--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -3,6 +3,7 @@ import 'package:add_to_cart_animation/add_to_cart_icon.dart';
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:green_grocery/src/config/custom_colors.dart';
+import 'package:green_grocery/src/pages/common_widgets/custom_shmmer.dart';
 import 'package:green_grocery/src/pages/home/components/item_tile.dart';
 import 'package:green_grocery/src/services/utils_services.dart';
 import '../common_widgets/app_name_widget.dart';
@@ -24,6 +25,19 @@ class _HomeTabState extends State<HomeTab> {
 
   void itemSelectedCartAnimations(GlobalKey gkImage) {
     runAddToCardAnimation(gkImage);
+  }
+
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      setState(() {
+        isLoading = false;
+      });
+    });
   }
 
   final UtilsServices utilsServices = UtilsServices();
@@ -96,44 +110,75 @@ class _HomeTabState extends State<HomeTab> {
               Container(
                 padding: const EdgeInsets.only(left: 25),
                 height: 40,
-                child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (_, index) {
-                    return CategoryTile(
-                      onPressed: () {
-                        setState(() {
-                          selectedCategory = appData.categories[index];
-                        });
-                      },
-                      category: appData.categories[index],
-                      isSelected: appData.categories[index] == selectedCategory,
-                    );
-                  },
-                  separatorBuilder: (_, index) => const SizedBox(
-                    width: 10,
-                  ),
-                  itemCount: appData.categories.length,
-                ),
+                child: !isLoading
+                    ? ListView.separated(
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (_, index) {
+                          return CategoryTile(
+                            onPressed: () {
+                              setState(() {
+                                selectedCategory = appData.categories[index];
+                              });
+                            },
+                            category: appData.categories[index],
+                            isSelected:
+                                appData.categories[index] == selectedCategory,
+                          );
+                        },
+                        separatorBuilder: (_, index) => const SizedBox(
+                          width: 10,
+                        ),
+                        itemCount: appData.categories.length,
+                      )
+                    : ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: List.generate(
+                            10,
+                            (index) => Container(
+                                  alignment: Alignment.center,
+                                  margin: const EdgeInsets.only(right: 12),
+                                  child: CustomShimmer(
+                                    height: 20,
+                                    width: 80,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                ))),
               ),
 
               // Grid
               Expanded(
-                child: GridView.builder(
-                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-                  physics: const BouncingScrollPhysics(),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
-                    childAspectRatio: 9 / 11.5,
-                  ),
-                  itemCount: appData.items.length,
-                  itemBuilder: (_, index) {
-                    return ItemTile(
-                        item: appData.items[index],
-                        cartAnimationMethod: itemSelectedCartAnimations);
-                  },
-                ),
+                child: !isLoading
+                    ? GridView.builder(
+                        padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                        physics: const BouncingScrollPhysics(),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 10,
+                          crossAxisSpacing: 10,
+                          childAspectRatio: 9 / 11.5,
+                        ),
+                        itemCount: appData.items.length,
+                        itemBuilder: (_, index) {
+                          return ItemTile(
+                              item: appData.items[index],
+                              cartAnimationMethod: itemSelectedCartAnimations);
+                        },
+                      )
+                    : GridView.count(
+                        padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                        physics: const BouncingScrollPhysics(),
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10,
+                        childAspectRatio: 9 / 11.5,
+                        children: List.generate(
+                            10,
+                            (index) => CustomShimmer(
+                                  height: double.infinity,
+                                  width: double.infinity,
+                                  borderRadius: BorderRadius.circular(20),
+                                ))),
               ),
             ],
           ),

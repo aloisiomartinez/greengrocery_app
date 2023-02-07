ae4d8b033c1b8e36a226ec63de215b91782d1689
aloisiomartinez <aloisiojacobs36@gmail.com>
2023-02-07 12:08:23 -0300

    homeRepository

--- lib/src/pages/home/repository/home_repository.dart
+++ lib/src/pages/home/repository/home_repository.dart
@@ -1,16 +1,27 @@
 import 'package:green_grocery/src/constants/endpoints.dart';
+import 'package:green_grocery/src/models/category_model.dart';
+import 'package:green_grocery/src/pages/home/result/home_result.dart';
 import 'package:green_grocery/src/services/http_manager.dart';
 
 class HomeRepository {
   final HttpManager _httpManager = HttpManager();
 
-  getAllCategories() async {
+  Future<HomeResult> getAllCategories() async {
     final result = await _httpManager.restRequest(
       url: Endpoints.getAllCategories,
       method: HttpMethods.post,
     );
 
     if (result['result'] != null) {
-    } else {}
+      List<CategoryModel> data =
+          (result['result'] as List<Map<String, dynamic>>)
+              .map(CategoryModel.fromJson)
+              .toList();
+
+      return HomeResult<CategoryModel>.success(data);
+    } else {
+      return HomeResult.error(
+          "Ocorreu um erro inesperado ao recuperar as categorias");
+    }
   }
 }
